2da9b7eafad8755b00314e434037af6d09af7e34
Joel Godoy <j.godoy.moraes@gmail.com>
2024-01-12 08:11:40 -0300

    Duplicated column fixed. Column name corrected

--- editor_planilha.py
+++ editor_planilha.py
@@ -1,13 +1,15 @@
 import pandas as pd
 #import openpyxl
 tabela = pd.read_excel("Produtos.xlsx")
-print(tabela)
+#print(tabela)
 
 # Modify the service tax
-tabela.loc[tabela["Tipo"]=="Serviço","Modificador Imposto"] =1.5
+tabela.loc[tabela["Tipo"]=="Serviço","Multiplicador Imposto"] =1.5
     
 # Calculate the updated price for the new service tax
-tabela["Preço Base Reais"] = tabela["Preço Base Original"]*tabela["Modificador Imposto"]
+tabela["Preço Base Reais"] = tabela["Preço Base Original"]*tabela["Multiplicador Imposto"]
+
+print(tabela)
 
 # Create an updated sheet without pandas indexes
 tabela.to_excel("ProdutosUpdated.xlsx",index=False)
